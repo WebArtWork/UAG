5ebf89bea22a92a835e44b5b39553cc443e7b137
Honchar Denys <crackeraki@gmail.com>
2025-12-12 19:49:31 +0200

    minor fix

---
+++ docs/docs.go
@@ -0,0 +1,8 @@
+package docs
+
+import "github.com/gorilla/mux"
+
+// RegisterOpenAPIService registers routes for the application's OpenAPI service.
+// For now this is a no-op placeholder to satisfy the application wiring until
+// proper documentation assets are added.
+func RegisterOpenAPIService(_ string, _ *mux.Router) {}
