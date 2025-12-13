ff1893d3ebad3a5b05367f04dde86f7dcc73aba9
Honchar Denys <crackeraki@gmail.com>
2025-12-13 09:39:22 +0200

    minor fixes

--- third_party/wasmd/api/wasm/module/v1/module.pb.go
+++ third_party/wasmd/api/wasm/module/v1/module.pb.go
@@ -1,10 +1,24 @@
 package modulev1
 
 import (
+	"fmt"
+
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	_ "google.golang.org/protobuf/reflect/protoreflect"
 )
 
 // Module is a minimal stand-in for the wasm module configuration proto.
 type Module struct {
-	UploadPermission interface{} `protobuf:"bytes,1,opt,name=upload_permission,json=uploadPermission,proto3" json:"upload_permission,omitempty"`
+	UploadPermission wasmtypes.AccessConfig `protobuf:"bytes,1,opt,name=upload_permission,json=uploadPermission,proto3" json:"upload_permission,omitempty"`
+}
+
+// Reset implements the proto.Message interface.
+func (m *Module) Reset() { *m = Module{} }
+
+// ProtoMessage implements the proto.Message interface.
+func (*Module) ProtoMessage() {}
+
+// String implements the proto.Message interface.
+func (m *Module) String() string {
+	return fmt.Sprintf("Module{UploadPermission:%v}", m.UploadPermission)
 }
